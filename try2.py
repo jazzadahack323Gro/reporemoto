952ecb27d6c78b9e21180c191cb8e5af9c179dca
jazzadahack323Gro <ada_hack323@hackademy.mx>
2022-03-04 15:43:09 -0600

    Second Branch Actualiza
    algunos cambios en archivo try

--- try2.py
+++ try2.py
@@ -1,8 +1,12 @@
 # All your code goes here!!!
 import random
 lst = [{'Nombre':"Eloisa",'Edad':21,'Temas':"Tech"},
-       {'Nombre':"Jamel",'Edad':34,'Temas':"Nature"},
-       {'Nombre':"Edna",'Edad':16,'Temas':"Oil"}]
+       {'Nombre':"Jamel",'Edad':34,'Temas':"Backend"},
+       {'Nombre':"Edna",'Edad':16,'Temas':"Mobile"},
+       {'Nombre':"Luis",'Edad':18,'Temas':"Frontend"},
+       {'Nombre':"Edgar",'Edad':15,'Temas':"React"},
+       {'Nombre':"Mariel",'Edad':21,'Temas':"Django"},
+       ]
 
 print("Directorio de estudiantes de SKILLS FOR WOMEN IN TECH")
 print(random.choice(lst))
